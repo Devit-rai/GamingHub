59893a439d3323566be117f683a10d3e11b0576c
Devit-rai <davidrai806@gmail.com>
2025-06-04 19:51:17 +0545

    Added components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,46 @@
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import Home from './pages/Home';
+import Products from './pages/Products';
+import ProductDetail from './pages/ProductDetail';
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
+import Cart from './pages/Cart';
+import Profile from './pages/Profile';
+import Categories from './pages/Categories';
+import PrivateRoute from './components/auth/PrivateRoute';
 
 function App() {
-
   return (
-    <>
-      <p className='text-3xl font-bold underline text-red-500'>Hello</p>
-    </>
-  )
+    <Router>
+      <div className="min-h-screen flex flex-col">
+        <Navbar />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/product/:id" element={<ProductDetail />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/categories" element={<Categories />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route
+              path="/profile"
+              element={
+                <PrivateRoute>
+                  <Profile />
+                </PrivateRoute>
+              }
+            />
+          </Routes>
+        </main>
+        <Footer />
+        <Toaster position="top-right" />
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
